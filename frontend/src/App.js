365089a9e07e14eb0c1bd907b247d453b906f730
Awais512 <raza.awais25@gmail.com>
2021-10-12 19:22:18 +0500

    Creating Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
-import Header from './components/Header';
+import Header from './components/layout/Header/Header';
+import WebFont from 'webfontloader';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Roboto', 'Droid Sans', 'Chilanka'],
+      },
+    });
+  }, []);
   return (
     <div>
       <Header />
