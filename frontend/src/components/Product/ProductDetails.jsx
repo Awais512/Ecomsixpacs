72357a4426c48e54dcbef2a1a5f00c5d3c5cc582
Awais512 <raza.awais25@gmail.com>
2021-10-14 16:47:57 +0500

    Product details Page Building 2

--- frontend/src/components/Product/ProductDetails.jsx
+++ frontend/src/components/Product/ProductDetails.jsx
@@ -3,7 +3,10 @@ import './ProductDetails.css';
 import Carousel from 'react-material-ui-carousel';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProductDetails } from '../../actions/productActions';
-import Rating from 'react-rating-stars-component';
+// import ReactStars from 'react-rating-stars-component';
+import { Rating } from '@material-ui/lab';
+import ReviewCard from './ReviewCard';
+import Loader from '../layout/Loader/Loader';
 
 const ProductDetails = ({ match }) => {
   const id = match.params.id;
@@ -24,69 +27,78 @@ const ProductDetails = ({ match }) => {
   };
   return (
     <>
-      <div className='ProductDetails'>
-        <div>
-          <Carousel>
-            {product.images &&
-              product.images.map((item, i) => (
-                <img
-                  className='CarouselImage'
-                  key={i}
-                  src={item.url}
-                  alt={`${i} Slide`}
-                />
-              ))}
-          </Carousel>
-        </div>
-
-        <div>
-          <div className='detailsBlock-1'>
-            <h2>{product.name}</h2>
-            <p>Product # {product._id}</p>
-          </div>
-          <div className='detailsBlock-2'>
-            <Rating {...options} />
-            <span className='detailsBlock-2-span'>
-              {' '}
-              ({product.numOfReviews} Reviews)
-            </span>
-          </div>
-          <div className='detailsBlock-3'>
-            <h1>{`₹${product.price}`}</h1>
-            <div className='detailsBlock-3-1'>
-              <div className='detailsBlock-3-1-1'>
-                <button>-</button>
-                <input readOnly type='number' />
-                <button>+</button>
-              </div>
-              <button disabled={product.Stock < 1 ? true : false}>
-                Add to Cart
-              </button>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className='ProductDetails'>
+            <div>
+              <Carousel>
+                {product.images &&
+                  product.images.map((item, i) => (
+                    <img
+                      className='CarouselImage'
+                      key={i}
+                      src={item.url}
+                      alt={`${i} Slide`}
+                    />
+                  ))}
+              </Carousel>
             </div>
 
-            <p>
-              Status:
-              <b className={product.Stock < 1 ? 'redColor' : 'greenColor'}>
-                {product.Stock < 1 ? 'OutOfStock' : 'InStock'}
-              </b>
-            </p>
+            <div>
+              <div className='detailsBlock-1'>
+                <h2>{product.name}</h2>
+                <p>Product # {product._id}</p>
+              </div>
+              <div className='detailsBlock-2'>
+                <Rating {...options} />
+                <span className='detailsBlock-2-span'>
+                  {' '}
+                  ({product.numOfReviews} Reviews)
+                </span>
+              </div>
+              <div className='detailsBlock-3'>
+                <h1>{`$${product.price}`}</h1>
+                <div className='detailsBlock-3-1'>
+                  <div className='detailsBlock-3-1-1'>
+                    <button>-</button>
+                    <input readOnly type='number' />
+                    <button>+</button>
+                  </div>
+                  <button>Add to Cart</button>
+                </div>
+
+                <p>
+                  Status:
+                  <b className={product.Stock < 1 ? 'redColor' : 'greenColor'}>
+                    {product.Stock < 1 ? 'OutOfStock' : 'InStock'}
+                  </b>
+                </p>
+              </div>
+
+              <div className='detailsBlock-4'>
+                Description : <p>{product.description}</p>
+              </div>
+
+              <button className='submitReview'>Submit Review</button>
+            </div>
           </div>
 
-          <div className='detailsBlock-4'>
-            Description : <p>{product.description}</p>
-          </div>
+          <h3 className='reviewsHeading'>REVIEWS</h3>
 
-          <button className='submitReview'>Submit Review</button>
-        </div>
-      </div>
-      <h3 className='reviewsHeading'>Reviews</h3>
-      {/* {product.reviews && product.reviews[0]?(
-          <div className="reviews">
-              {product.reviews && product.reviews.map(review)=> (
-                  <ReviewCard />
-              )}
-          </div>
-      ):() } */}
+          {product.reviews && product.reviews[0] ? (
+            <div className='reviews'>
+              {product.reviews &&
+                product.reviews.map((review) => (
+                  <ReviewCard key={review._id} review={review} />
+                ))}
+            </div>
+          ) : (
+            <p className='noReviews'>No Reviews Yet</p>
+          )}
+        </>
+      )}
     </>
   );
 };
